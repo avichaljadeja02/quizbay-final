9c3c607895a11d59843d375ba3802c70a0414de5
Nikunjkumar Patel <nikunjkumarpatel@Nikunjs-MacBook-Pro.local>
2021-01-25 11:34:40 +0530

    Android Update

---
+++ QuizBay/app/src/main/java/com/example/myapplication/quinbay/quizbay/quizbay/PastQuizesActivity.java
@@ -0,0 +1,16 @@
+package com.example.myapplication.quinbay.quizbay.quizbay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication.quinbay.quizbay.R;
+
+public class PastQuizesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_past_quizes);
+    }
+}
